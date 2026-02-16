a9250dd68b78d66a2ea391854a4d13c217ca0c91
Victoria Bruhn <vbruhn.incoming@fh-salzburg.ac.at>
2026-02-16 10:29:53 +0100

    Livekodning

--- AverageDayTemp/Program.cs
+++ AverageDayTemp/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+
+            // Det jeg skrev først
+            /*
             int[] temperature = { 7, 4, 5, 5, 5, 4, 4, 4 };
 
             Console.WriteLine("Her er de forskellige temperaturer i løbet af ugen: ");
@@ -27,7 +30,28 @@
             { 
 
             }
+            */
 
+            //Live kodning i klassen
+            double[] vejr = { 7, 4, 5, 5, 5, 4, 4, 4 };
+
+            double avgTemp = 0;
+
+            for (int i = 0; i < vejr.Length; i++)
+            { 
+                avgTemp += vejr[i];
+                Console.WriteLine(avgTemp);
+            }
+
+            Console.WriteLine("Gennemsnitstemperatur: ");
+            avgTemp = avgTemp / vejr.Length;
+            Console.WriteLine(avgTemp);
+
+            //Sort array
+            vejr.Sort();
+            Console.WriteLine($"Den laveste temperatur er: {vejr[0]}");
+
+            Console.WriteLine($"og den højeste temperatur er: {vejr[7]}");
 
         }
 
